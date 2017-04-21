0bcb7234e38ce1121d5615c0b25c7a421b7452e9
mengxaingfeng <xiangfeng.meng@nollec.com>
2017-04-21 15:39:41 +0800

    Initial Commit

---
+++ onlyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  onlyDemo
+//
+//  Created by jingshan on 2017/4/21.
+//  Copyright © 2017年 jingshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ onlyDemoTests/onlyDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  onlyDemoTests.m
+//  onlyDemoTests
+//
+//  Created by jingshan on 2017/4/21.
+//  Copyright © 2017年 jingshan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface onlyDemoTests : XCTestCase
+
+@end
+
+@implementation onlyDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
