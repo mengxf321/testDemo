5df96335bd75c8eae34a0104603ff3aa3f3f0247
mengxaingfeng <xiangfeng.meng@nollec.com>
2017-04-21 15:50:56 +0800

    anyway

--- onlyDemo/ViewController.m
+++ onlyDemo/ViewController.m
@@ -19,7 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.view.backgroundColor = [UIColor whiteColor];
-    self.title = @"测试";
+    self.title = @"hello";
+    
 }
 
 
